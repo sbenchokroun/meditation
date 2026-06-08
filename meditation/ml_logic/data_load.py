4495186f0e8b62145002a14756334815dbb24210
Richard Tring <tring.richard@hotmail.fr>
2026-06-08 10:57:01 +0200

    Added data load one subject and train val test split

--- meditation/ml_logic/data_load.py
+++ meditation/ml_logic/data_load.py
@@ -34,16 +34,7 @@ def slice_data(data, window_size=1000, step=1000, start=0) -> list:
             index = index + step
         return data_sliced
 
-def load_data_dict(paths=dict, window_size=1000, step=1000, start=0):
-
-    """"
-    - renvoie un tuple contenant X de shape (nb de seq, window_size, nb de canaux)
-    et y de shape (nb de seq,) à partir d'un dictionnaire de path.
-    - start corresponds au début de la 1ère séquence
-
-    """
-
-    def is_medit(key=str):
+def is_medit(key=str):
 
         """renvoie 0 (repos) ou 1 (méditation) selon le label"""
 
@@ -56,6 +47,15 @@ def load_data_dict(paths=dict, window_size=1000, step=1000, start=0):
             print(f'label {label} does not exist')
             return None
 
+
+def load_data_dict(paths=dict, window_size=1000, step=1000, start=0):
+
+    """"
+    - renvoie un tuple contenant X de shape (nb de seq, window_size, nb de canaux)
+    et y de shape (nb de seq,) à partir d'un dictionnaire de path.
+    - start corresponds au début de la 1ère séquence
+
+    """
 
     X_final = []
     y_final = []
@@ -114,3 +114,98 @@ def load_data(sujets=None,
     return load_data_dict(path_data(sujets=sujets, labels=labels, sessions=sessions, root=root),
                           window_size=window_size, step=step,
                           start=start)
+
+def load_one_subject(sujet=int,
+                     labels=['Medita', 'slMedita', 'restCE01', 'restCE02', 'restOE'],
+                     sessions=['premedita'],
+                     window_size=1000,
+                     step=1000,
+                     start=0,
+                     root=ROOT,
+                     train_split=float,
+                     gap=0
+                     ):
+
+    # Get data path
+    paths = path_data([sujet], labels, sessions, root)
+
+    X_train_final = []
+    X_test_final = []
+    y_train_final = []
+    y_test_final = []
+
+    for key, path in paths.items():
+        # Load data
+        data = np.load(path).T
+
+        # Split data (temporally) between train and test, with gap between train and test
+        data_train = data[:int(train_split*data.shape[0]),:]
+        data_test = data[int(train_split*data.shape[0]) + gap:,:]
+
+        # Apply windowing to build X, y
+        X_train = np.array(slice_data(data_train, window_size=window_size, step=step, start=start))
+        y_train = np.array([is_medit(key) for i in range(len(X_train))])
+        X_test = np.array(slice_data(data_test, window_size=window_size, step=step, start=0))
+        y_test = np.array([is_medit(key) for i in range(len(X_test))])
+
+        # Stack the resulting windows
+        X_train_final.extend(X_train)
+        y_train_final.extend(y_train)
+        X_test_final.extend(X_test)
+        y_test_final.extend(y_test)
+
+    return np.array(X_train_final), np.array(X_test_final), np.array(y_train_final), np.array(y_test_final)
+
+def load_train_val_test_one_subject(sujet=int,
+                                    labels=['Medita', 'slMedita', 'restCE01', 'restCE02', 'restOE'],
+                                    sessions=['premedita'],
+                                    window_size=1000,
+                                    step=1000,
+                                    start=0,
+                                    root=ROOT,
+                                    train_split=0.8,
+                                    val_split=0.2,
+                                    gap_train_val=0,
+                                    gap_train_test=0
+                                    ):
+
+    # Get data path
+    paths = path_data([sujet], labels, sessions, root)
+
+    X_train_final = []
+    X_test_final = []
+    X_val_final = []
+    y_val_final = []
+    y_train_final = []
+    y_test_final = []
+
+    for key, path in paths.items():
+        # Load data
+        data = np.load(path).T
+
+        # Get train and val slice
+        train_slice = int(train_split*data.shape[0])
+        val_slice = int(train_slice*val_split)
+
+        # Split data (temporally) between train, val and test, with gap between train, val and test
+        data_train = data[: train_slice - val_slice,:]
+        data_val = data[train_slice - val_slice + gap_train_val: train_slice,:]
+        data_test = data[train_slice + gap_train_test:,:]
+
+        # Apply windowing to build X, y
+        X_train = np.array(slice_data(data_train, window_size=window_size, step=step, start=start))
+        y_train = np.array([is_medit(key) for i in range(len(X_train))])
+        X_val = np.array(slice_data(data_val, window_size=window_size, step=step, start=0))
+        y_val = np.array([is_medit(key) for i in range(len(X_val))])
+        X_test = np.array(slice_data(data_test, window_size=window_size, step=step, start=0))
+        y_test = np.array([is_medit(key) for i in range(len(X_test))])
+
+        # Stack the resulting windows
+        X_train_final.extend(X_train)
+        y_train_final.extend(y_train)
+        X_val_final.extend(X_val)
+        y_val_final.extend(y_val)
+        X_test_final.extend(X_test)
+        y_test_final.extend(y_test)
+
+    return np.array(X_train_final), np.array(X_val_final), np.array(X_test_final), np.array(y_train_final), np.array(y_val_final), np.array(y_test_final)
